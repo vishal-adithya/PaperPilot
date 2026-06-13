0257c6a7c4ee865f089547b972832ea7dff17068
Vishal Adithya.A <vishaladithya2008@gmail.com>
2026-06-13 16:06:22 +0530

    First Commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import arxiv
+from langgraph.graph import StateGraph
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph.message import add_messages
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
